b6e3ab539747c8c043d91dcc5b56c957b4a457be
austinmicroexcel <austin.bagley@microexcel.com>
2019-08-26 11:38:00 -0600

    remove extra files, clean up comments

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -3,11 +3,9 @@ import { Routes } from "@angular/router";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 
 import { HomeComponent } from "./home.component";
-import { ItemDetailComponent } from "./item-detail/item-detail.component";
 
 const routes: Routes = [
     { path: "default", component: HomeComponent },
-    { path: "item/:id", component: ItemDetailComponent }
 ];
 
 @NgModule({
